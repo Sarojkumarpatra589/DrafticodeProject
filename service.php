81bde5842dec7b62da86fc021f16b4967cf9e4e5
Sarmistha <sarmistha@gmail.com>
2026-03-21 16:03:54 +0530

    updated

--- service.php
+++ service.php
@@ -47,6 +47,11 @@ $settings = $stmt->fetch(PDO::FETCH_ASSOC);
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 </head>
+<style>
+     .content-box .text-service p{
+        color:white;
+    }
+</style>
 <body>
 <div class="page-wrapper">
 <!-- Preloader -->
@@ -71,7 +76,7 @@ $settings = $stmt->fetch(PDO::FETCH_ASSOC);
                         <h1 class="title">Services</h1>
                       </div>
                       <ul class="breadcume-pull">
-                        <li><a class="title-line" href="index-2.html">Home <span><i class="fas fa-angle-right"></i></span></a></li>
+                        <li><a class="title-line" href="index.php">Home <span><i class="fas fa-angle-right"></i></span></a></li>
                         <li>Services</li>
                       </ul>
                     </div>
@@ -91,70 +96,57 @@ include 'connection/config.php';
 $services = $pdo->query("SELECT * FROM services ORDER BY id DESC")->fetchAll();
 ?>
 
-<!-- Service Section -->
 <section class="service-section">
     <div class="bg bg-pattern-2"></div>
+
     <div class="auto-container">
-       
+        <div class="row g-4">
 
-        <div class="outer-box">
-            <div class="swiper service-swiper">
-                <div class="swiper-wrapper">
+            <?php foreach ($services as $service): ?>
 
-                    <?php foreach ($services as $service): ?>
+            <div class="col-lg-4 col-md-6">
+                <div class="service-block">
+                    <div class="inner-box">
 
-                    <!-- Service Block -->
-                    <div class="service-block swiper-slide">
-                        <div class="inner-box">
-                            <div class="image-box">
-                                <figure class="image">
-                                    <a href="service_details.php?slug=<?= $service['slug'] ?>">
-                                        <img src="upload/<?= htmlspecialchars($service['image']) ?>" alt="Image">
-                                    </a>
-                                </figure>
-                                <div class="icon-box">
-                                    <i class="icon flaticon-health-check"></i>
-                                </div>
-                            </div>
+                        <div class="image-box">
+                            <figure class="image">
+                                <a href="service_details.php?slug=<?= $service['slug'] ?>">
+                                    <img src="upload/<?= htmlspecialchars($service['image']) ?>" alt="Image" class="img-fluid">
+                                </a>
+                            </figure>
 
-                            <div class="content-box">
-                                <h4 class="title">
-                                    <a href="service_details.php?slug=<?= $service['slug'] ?>">
-                                        <?= htmlspecialchars($service['title']) ?>
-                                    </a>
-                                </h4>
-
-                                <div class="text">
-                                    <?= $service['short_description'] ?>
-                                </div>
-
-                                <div class="btn-box">
-                                    <a href="service_details.php?slug=<?= $service['slug'] ?>" class="readmore">
-                                        Discover More
-                                    </a>
-                                </div>
+                            <div class="icon-box">
+                                <i class="icon flaticon-health-check"></i>
                             </div>
                         </div>
+
+                        <div class="content-box">
+                            <h4 class="title">
+                                <a href="service_details.php?slug=<?= $service['slug'] ?>">
+                                    <?= htmlspecialchars($service['title']) ?>
+                                </a>
+                            </h4>
+
+                            <div class="text text-service">
+                                <?= $service['short_description'] ?>
+                            </div>
+
+                            <div class="btn-box">
+                                <a href="service_details.php?slug=<?= $service['slug'] ?>" class="readmore">
+                                    Discover More
+                                </a>
+                            </div>
+                        </div>
+
                     </div>
-
-                    <?php endforeach; ?>
-
                 </div>
             </div>
 
-            <!-- Navigation -->
-            <div class="nav-box">
-                <div class="swiper-button-prev">
-                    <span class="icon fal fa-long-arrow-left"></span>
-                </div>
-                <div class="swiper-button-next">
-                    <span class="icon fal fa-long-arrow-right"></span>
-                </div>
-            </div>
+            <?php endforeach; ?>
+
         </div>
     </div>
 </section>
-<!-- End Service Section -->
 
 
 
